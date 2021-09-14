0514dcca26f735656875008f8fed0bd37933c254
keinelain <laineytubbs@protonmail.com>
2021-09-13 19:13:52 -0500

    landing page works

--- myportfolio2/projects/models.py
+++ myportfolio2/projects/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django import forms
+from django.contrib.auth.models import User
+from django.urls import reverse,NoReverseMatch  
 # Create your models here.
+
+STATUS = (
+	(0, "Working!"),
+	(1, "Finished")
+	)
+
+
+class Project(models.Model):
+	title = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200)
+	projectlink = models.URLField(max_length=200)
+	language = models.TextField(max_length=40)
+	updated_on = models.DateTimeField(auto_now=True)
+	about = models.TextField()
+	status = models.IntegerField(choices=STATUS, default=0)
+
+	# def get_absolute_url(self):
+ #    	return reverse('postDetail', kwargs={'slug': self.slug}) 
+    # slug = models.SlugField(max_length=200)
+    
+    # projectlink = models.URLField(**kwargs)
+    # language = models.TextField()
+    # about = models.TextField()
+    # created_on = models.DateTimeField(auto_now_add=True)
+    # status = models.IntegerField(choices=STATUS, default=0)
+    # language = models.TextField()
--- myportfolio2/projects/views.py
+++ myportfolio2/projects/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+deom .models import Project
 from django.urls import reverse
 
 
 # Create your views here.
 
 def projectIndex(request):
-	return HttpResponse("Index for the project pages")
-def Moddy(request):
-	return HttpResponse("This will be the page about Moddy")
-def Auris(request):
-	return HttpResponse("This will be the page for Auris")
+	projects = Project.objects.all()
+	context = {
+		"projects" : projects,
+			}
+	return render(request, 'projects/project_index.html',context)
+def project
