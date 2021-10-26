b42b5da8254dec27dd4da4a42abc3e5f6c47868f
keinelain <laineytubbs@protonmail.com>
2021-10-25 22:22:17 -0500

    switch to class based view

--- myportfolio2/projects/urls.py
+++ myportfolio2/projects/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import include , path
-from . import views
+from .views import projectIndex 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 urlpatterns = [
-    path('',views.projectIndex, name='projectIndex'),
+    path('',projectIndex.as_view(), name='projectIndex'),
     # path('<slug:slug>/', views.projectDetail,name='project detail')
     # path('Auris/', views.Auris,name='Auris info' ),
     # path('Moddy/', views.Moddy, name='Moddy info'),
--- myportfolio2/projects/views.py
+++ myportfolio2/projects/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
 from django.urls import reverse
-
+from django.views.generic.list import ListView
 
 # Create your views here.
 
-def projectIndex(request):
-	projects = Project.objects.all()
-	context = {
-		"projects" : projects,
-			}
-	return render(request, 'projects/project_index.html',context)
+# def projectIndex(request):
+# 	projects = Project.objects.all()
+# 	context = {
+# 		"projects" : projects,
+# 			}
+# 	return render(request, 'projects/project_index.html',context)
+
+class projectIndex(ListView):
+	model = Project
+	context_object_name = 'projects'
+	template_name = 'projects/project_index.html'
+	
 # def projectDetail(request):
 # 	projects = Project.objects()
 # 	context = {
